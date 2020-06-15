82ac83da7105f26c73869d92a8c4ee0e730f1494
vcokltfre <vcokltfre@gmail.com>
2020-06-16 00:06:57 +0100

    feat: Added on_ready functionality to general

--- cogs/general.py
+++ cogs/general.py
@@ -2,8 +2,12 @@ import discord
 from discord.ext import commands
 
 class General(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        await self.bot.change_presence(activity=discord.Game(name="TriggaBot"))
 
 def setup(bot):
     bot.add_cog(General(bot))
